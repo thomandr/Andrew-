729598b1383be798ab07d28d8914219280944b5c
thomandr <59656875+thomandr@users.noreply.github.com>
2020-01-24 23:01:04 -0500

    Assignment1

---
+++ Assignment1/polynomial.h
@@ -0,0 +1,5 @@
+#ifndef polynomial_h
+#define polynomial_h
+
+void polynomial(double a, double b, double c, double x, double xi, double *fPrime, double *f, double *area); // Prototyping the function
+#endif //polynomial
